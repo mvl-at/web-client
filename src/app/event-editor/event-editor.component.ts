4df513681bfd014bf9b38c3720343058537cf776
Richard Stöckl <richard.stoeckl@aon.at>
2018-09-03 10:26:44 +0200

    fixed date and time format in event-editor

--- src/app/event-editor/event-editor.component.ts
+++ src/app/event-editor/event-editor.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Event} from '../events/events.component';
-import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbActiveModal, NgbDateAdapter, NgbDateNativeAdapter, NgbTimeAdapter, NgbTimeStruct} from '@ng-bootstrap/ng-bootstrap';
 import {Editor} from '../app.component';
 import {DataService} from '../rest/data-service';
 import {NgbDate} from '@ng-bootstrap/ng-bootstrap/datepicker/ngb-date';
+import {NgbTime} from '@ng-bootstrap/ng-bootstrap/timepicker/ngb-time';
+
+export class JSONTimeAdapter extends NgbTimeAdapter<Date> {
+
+  fromModel(value: Date): NgbTimeStruct {
+    let time = null;
+    if (value !== null) {
+      time = {hour: value.getHours(), minute: value.getMinutes(), second: value.getSeconds()};
+    }
+    return time;
+  }
+
+  toModel(time: NgbTimeStruct): Date {
+    const date = new Date();
+    if (time != null) {
+      date.setHours(time.hour, time.minute, time.second);
+    }
+    return date;
+  }
+
+}
 
 @Component({
   selector: 'app-event-management',
   templateUrl: './event-editor.component.html',
-  styleUrls: ['./event-editor.component.css']
+  styleUrls: ['./event-editor.component.css'],
+  providers: [{provide: NgbDateAdapter, useClass: NgbDateNativeAdapter}, {provide: NgbTimeAdapter, useClass: JSONTimeAdapter}]
 })
 export class EventEditorComponent extends Editor<Event> implements OnInit {
-
-  private now: Date = new Date(Date.now());
-  private date: NgbDate = new NgbDate(this.now.getFullYear(), this.now.getMonth(), this.now.getDate());
 
   constructor(public activeModal: NgbActiveModal, public service: DataService) {
     super(activeModal, service);
@@ -23,8 +42,10 @@ export class EventEditorComponent extends Editor<Event> implements OnInit {
   }
 
   defaults(): Event {
-    return {id: null, date: null, internal: false, important: false, name: null,
-      musicianTime: null, musicianPlace: null, note: null, place: null, time: null, uniform: null};
+    return {
+      id: null, date: null, internal: false, important: false, name: null,
+      musicianTime: null, musicianPlace: null, note: null, place: null, time: null, uniform: null
+    };
   }
 
   processedEntity(): Event {
