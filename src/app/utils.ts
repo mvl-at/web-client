32712b34d3f8fa8c635f0e38ddd62e028bcd90f7
Richard Stöckl <richard.stoeckl@aon.at>
2018-09-24 11:05:26 +0200

    added supoort for date strings only

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -10,12 +10,16 @@ export class Utils {
 
   formatDate(date: string, time: string): string {
     const dtime = new Date(Date.parse(time));
+    const ddate = new Date(Date.parse(date));
 
     if (date === undefined || date === null) {
       return dtime.toLocaleString(undefined, {hour: 'numeric', minute: 'numeric'});
     }
 
-    const ddate = new Date(Date.parse(date));
+    if (time === undefined || time === null) {
+      return ddate.toLocaleString(undefined, {day: 'numeric', month: 'numeric', year: 'numeric'});
+    }
+
     const combinedDate = new Date();
     combinedDate.setFullYear(ddate.getFullYear(), ddate.getMonth(), ddate.getDate());
     combinedDate.setHours(dtime.getHours(), dtime.getMinutes());
