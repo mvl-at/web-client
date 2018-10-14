11314d0d9f3dbacf1666aeffa133711720ef742a
Richard Stöckl <richard.stoeckl@aon.at>
2018-10-14 11:31:41 +0200

    sorted and filtered members

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {DataService} from '../rest/data-service';
 
@@ -12,21 +12,56 @@ export class MembersComponent implements OnInit {
   members: Member[] = [];
   instruments: Instrument[] = [];
 
-  constructor(private http: HttpClient, private service: DataService) { }
+  membersInstruments = new Map<number, Member[]>();
+  firstFetchDone = false;
 
-  ngOnInit() {
-    this.service.getMembers().subscribe(m => this.members = m);
-    this.service.getInstruments().subscribe(i => this.instruments = i);
+  constructor(private http: HttpClient, private service: DataService) {
   }
 
-  membersByInstrument(instrument: Instrument) {
+  ngOnInit() {
+    this.service.getMembers().subscribe(m => {
+      this.members = m.filter(me => me.active);
+      this.relations();
+    });
+    this.service.getInstruments().subscribe(i => {
+      this.instruments = i.sort((a, b) => (a.name > b.name) ? 1 : -1);
+      this.relations();
+    });
+  }
+
+  membersByInstrument(instrument: Instrument): Member[] {
     let m: Member[] = [];
     this.members.forEach(function (member) {
       if (member.instrumentId === instrument.id) {
         m = m.concat(member);
       }
     });
+    m.sort((a, b) => {
+      const joinedDiff = a.joined - b.joined;
+      if (joinedDiff !== 0) {
+        return joinedDiff;
+      }
+      return (a.lastName > b.lastName) ? 1 : -1;
+    });
     return m;
+  }
+
+  membersFromInstrument(instrument: Instrument): Member[] {
+    return this.membersInstruments.has(instrument.id) ? this.membersInstruments.get(instrument.id) : [];
+  }
+
+  relations() {
+    if (this.firstFetchDone) {
+      this.instruments.forEach(i => this.membersInstruments.set(i.id, this.membersByInstrument(i)));
+      this.instruments = this.instruments.filter((i) => this.membersInstruments.get(i.id).length > 0);
+      this.membersInstruments.forEach((v, k) => {
+        if (v.length === 0) {
+          this.membersInstruments.delete(k);
+        }
+      });
+    } else {
+      this.firstFetchDone = true;
+    }
   }
 }
 
