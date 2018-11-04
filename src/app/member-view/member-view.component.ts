05309b4810151d67eaaa1967a496d8e7dc3e96f0
Richard Stöckl <richard.stoeckl@aon.at>
2018-11-04 17:48:26 +0100

    added bier-broesel

--- src/app/member-view/member-view.component.ts
+++ src/app/member-view/member-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, NgZone, OnInit} from '@angular/core';
 import {Member} from '../members/members.component';
 import {LeaderRoleMember} from '../leaders/leaders.component';
 import {DataService} from '../rest/data-service';
@@ -10,19 +10,39 @@ import {DataService} from '../rest/data-service';
 })
 export class MemberViewComponent implements OnInit {
 
+  static cantinaMode = false;
+  static audio: HTMLAudioElement;
+
   @Input() member: Member;
   @Input() leader: LeaderRoleMember;
 
-  constructor(public service: DataService) {
+  constructor(public service: DataService, private ngZone: NgZone) {
+    MemberViewComponent.audio = new Audio('assets/bier-broesel.ogg');
+    MemberViewComponent.audio.load();
+    MemberViewComponent.audio.addEventListener('ended', () => (MemberViewComponent.cantinaMode = false));
+  }
+
+  cantina() {
+    // @ts-ignore
+    this.ngZone.run(() => {
+      MemberViewComponent.cantinaMode = !MemberViewComponent.cantinaMode;
+      MemberViewComponent.audio.play();
+    });
   }
 
   ngOnInit() {
     if (this.leader !== null && this.leader !== undefined) {
       this.member = this.leader.member;
     }
+    // @ts-ignore
+    window.cantina = this.cantina.bind(this);
   }
 
   dummyPicture(event, member: Member) {
     event.target.src = '/assets/icons/avatar-' + ((member.gender === 'f') ? 'female' : 'male') + '.png';
+  }
+
+  isCantina(): boolean {
+    return MemberViewComponent.cantinaMode;
   }
 }
