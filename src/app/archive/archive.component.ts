cd542ab7a35ad5c53df606408434218a7b5b113e
Richard Stöckl <richard.stoeckl@aon.at>
2018-12-23 20:37:09 +0100

    added search and pagination in archive

--- src/app/archive/archive.component.ts
+++ src/app/archive/archive.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {List} from '../list';
 import {DataService} from '../rest/data-service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {compareNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_version';
 import {ArchiveEditorComponent} from '../archive-editor/archive-editor.component';
+import {Utils} from '../utils';
 
 @Component({
   selector: 'app-archive',
@@ -12,11 +12,19 @@ import {ArchiveEditorComponent} from '../archive-editor/archive-editor.component
 })
 export class ArchiveComponent extends List<Archive> implements OnInit {
 
+  itemAmounts = [5, 10, 20, 50, 100];
+  itemAmount: number;
   sortField = 'title';
   ascending = true;
+  search = '';
+  page = 1;
 
-  constructor(service: DataService, modal: NgbModal) {
+  displayItems = [];
+  pagedItems = [];
+
+  constructor(service: DataService, modal: NgbModal, private utils: Utils) {
     super(service, modal);
+    this.itemAmount = 5;
   }
 
   ngOnInit() {
@@ -35,12 +43,41 @@ export class ArchiveComponent extends List<Archive> implements OnInit {
   }
 
   onLoaded() {
-    this.items.sort((a, b) => this.compareArchive(a, b));
+    this.displayItems = this.items.sort((a, b) => this.compareArchive(a, b));
+    this.slice();
   }
 
   sort(property: string) {
-    this.ascending = (this.sortField === property) !== this.ascending;
+    this.ascending = !((this.sortField === property) && this.ascending);
     this.sortField = property;
+    this.onLoaded();
+  }
+
+  filter() {
+    this.displayItems = this.items.filter(a => this.contains(a));
+    this.page = 1;
+    this.slice();
+  }
+
+  contains(a: Archive): boolean {
+    const cstring = this.utils.accentFold(this.search);
+    return this.utils.accentFold(a.title).includes(cstring) ||
+      this.utils.accentFold(a.composer).includes(cstring) ||
+      this.utils.accentFold(a.arranger).includes(cstring) ||
+      this.utils.accentFold(a.style).includes(cstring) ||
+      this.utils.accentFold(a.publisher).includes(cstring) ||
+      this.utils.accentFold(a.location).includes(cstring);
+  }
+
+  slice() {
+    const startItem = (this.page - 1) * this.itemAmount;
+    console.log(startItem + ' - ' + (startItem + this.itemAmount));
+    this.pagedItems = this.displayItems.slice(startItem, startItem + this.itemAmount);
+  }
+
+  changedPages() {
+    this.itemAmount = parseInt(this.itemAmount.toString(), 10);
+    console.log('selected');
     this.onLoaded();
   }
 }
